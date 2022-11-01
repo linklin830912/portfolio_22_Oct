a001205f7047fb36c0d408745e68683fc45daf92
linklin830912 <56695482+linklin830912@users.noreply.github.com>
2022-11-02 00:31:23 +0800

    uiux partial done

--- js/robotic0.js
+++ js/robotic0.js
@@ -1,10 +1,15 @@
 var dy_mode = "swing";
+var moveIndex = -1;
+var rect;
+var rect0;
+var rect1;
+
 mouseOnVideoAnimation();
+mouseOnCanvasAnimation();
 
 function mouseOnVideoAnimation(){
     $(".robotic_video").on("mouseenter", mouseEnterVideo);
     $(".robotic_video").on("mouseleave", mouseLeaveVideo);
-
 }
 function mouseEnterVideo(){
     let id = $(this).attr("id");
@@ -18,8 +23,40 @@ function mouseEnterVideo(){
 function mouseLeaveVideo(){
     let id = $(this).attr("id");
     $("#"+id).css("filter", "grayscale(80%)");
-    
+
     let element = $("#"+id)[0];
 
     element.pause();
+}
+
+function mouseOnCanvasAnimation(){
+    rect0 = $("#ui0_canvas")[0].getBoundingClientRect();
+    rect1 = $("#ui1_canvas")[0].getBoundingClientRect();
+
+    $(".ui_img").on("mouseenter", miniMouseEnter);
+    $(".ui_img").on("mousemove", miniMouseMove);
+}
+function miniMouseEnter(){
+    let id = $(this).attr("id");
+    if(id.includes("0")){
+        rect = {
+            "left": rect0.left-10,
+            "top": rect0.top-60
+        };
+    }else if(id.includes("1")){
+        rect = {
+            "left": rect1.left+20,
+            "top": rect1.top-180
+        };
+    }
+}
+
+function miniMouseMove(){
+    let id = $(this).attr("id");
+    let canvasId = id.replace("img", "canvas");
+    let canvas = $("#"+canvasId);
+     
+    canvas.css("left", event.clientX-rect.left);
+    canvas.css("top", event.clientY-rect.top);
+    
 }
