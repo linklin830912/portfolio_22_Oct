5128ca14db4f20847f4f71ea65420044abef0a12
linklin830912 <56695482+linklin830912@users.noreply.github.com>
2022-11-01 08:21:00 +0800

    subtle change

--- js/robotic0.js
+++ js/robotic0.js
@@ -1,19 +1,25 @@
+var dy_mode = "swing";
 mouseOnVideoAnimation();
 
 function mouseOnVideoAnimation(){
     $(".robotic_video").on("mouseenter", mouseEnterVideo);
     $(".robotic_video").on("mouseleave", mouseLeaveVideo);
+
 }
 function mouseEnterVideo(){
     let id = $(this).attr("id");
-    let element = $("#"+id)[0];
+    $("#"+id).css("filter", "grayscale(0%)");
+
+    let element = $("#"+id)[0];    
     
     element.currentTime = 0;
     element.play();
 }
 function mouseLeaveVideo(){
     let id = $(this).attr("id");
-    let element = $("#"+id)[0];
+    $("#"+id).css("filter", "grayscale(80%)");
     
+    let element = $("#"+id)[0];
+
     element.pause();
 }
